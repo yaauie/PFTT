c430b3e6d0f43c7feb70c97bbfb861f0e8cb6a74
Ryan Biesemeyer <ryan@yaauie.com>
2011-08-09 13:10:05 -0700

    added missing files

---
+++ lib/phpt_test_case.rb
@@ -0,0 +1,144 @@
+class PhptTestCase
+
+  include TestBenchFactor
+
+  @@supported_sections = {
+    :required => [
+      [:file,:fileeof,:file_external],
+      [:expect,:expectf,:expectregex],
+      [:test]
+    ],
+    :optional => [
+      [:post,:post_raw],
+      [:credit,:credits]
+    ]
+  }
+
+  attr_reader :phpt_path
+
+  def initialize( path )
+    if !File.exists?( path ) 
+      raise 'File not found: ['+path+']'
+    end
+    @phpt_path = path
+  end
+
+  def name
+    File.basename @phpt_path, '.phpt'
+  end
+
+  def description
+    @phpt_path
+  end
+
+  def options
+    require 'yaml'
+    if @options.nil?
+      @options = YAML::load parts[:pftt] if parts[:pftt]
+      @options ||= {}
+    end
+    @options
+  end
+
+  def ini
+    @ini ||= PhpIni.new parts[:ini]
+  end
+
+  def bork_reasons
+    borked?
+    @bork_reasons
+  end
+
+  def borked?
+    unless @bork_reasons
+      @bork_reasons = []
+      (@@supported_sections.values.flatten(1)).each do |group|
+        if (parts.keys & group).length > 1
+          @bork_reasons << 'duplicate sections:'+group.to_s
+        end
+      end
+      @@supported_sections[:required].each do |group|
+        if (parts.keys & group).length < 1;
+          @bork_reasons << 'missing required section:'+group.to_s
+        end
+      end
+    end
+    !@bork_reasons.length.zero?
+  end
+
+  def unsupported?
+    # are any sections in parts not present in @supported_sections
+    !(parts.keys - @@supported_sections.values.flatten).length.zero?
+  end
+
+  def get_section( section )
+    return parts[section] || nil
+  end
+
+  def save_section( section, path, extension=section.to_s )
+    fullpath = File.join( path, "#{name}.#{extension}")
+    File.open fullpath, 'w' do |file|
+      file.write get_section section
+    end
+    fullpath
+  end
+
+  def attach_result( result )
+    @result_tester.test( result ) unless @result_tester.raw == result
+  end
+
+  def pass?
+    raise 'Result not attached' if @result_tester.nil?
+    @result_tester.pass?
+  end
+
+  protected
+
+  def parse!
+    @raw ||= IO.read(@phpt_path)
+    reset!
+    @result_tester = nil
+    section = :none
+    @raw.lines do |line|
+      if line =~ /^--(?<section>[A-Z_]+)--/
+        section = Regexp.last_match[:section].downcase.to_sym
+        @parts[section]=''
+      else
+        @parts[section] += parse_line line, File.dirname( @phpt_path )
+      end
+    end
+  end
+
+  def reset!
+    @parts = {}
+    @ini = nil
+    @options = nil
+  end
+
+  private
+
+  def parse_line( line, context )
+    return line unless line =~ /^\#\!?include (?<script>.*)/
+    script = File.expand_path Regexp.last_match[:script].chomp, context
+    expanded = ''
+    IO.read(script).lines do |line|
+      expanded += parse_line line, File.dir_name(script)
+    end
+    expanded
+  end
+
+  def parts
+    parse! unless @parts
+    @parts
+  end
+end
+
+def PhptTestCase::Error
+  def initialize(message=nil)
+    @message = message
+  end
+
+  def to_s
+    @message
+  end
+end
---
+++ lib/test_bench.rb
@@ -0,0 +1,76 @@
+class TestBench
+  def initialize( php_build, host, middleware )
+    @php = php_build
+    @host = host
+    @middleware = middleware
+  end
+
+  # 
+  # Test each of the supplied factors to see if they are compatible with the others.
+  # This is a little tricky, as incompatibility is not necessarily two-way.
+  # 
+  # See TestBenchFactor::compatible? for conventions on how these are defined.
+  # 
+  def compatible?
+    [php,host,middleware].permutation(2) do |factor1,factor2|
+      return false if factor1.compatible_with? factor2
+    end
+    return true
+  end
+
+  attr_reader :php, :host, :middleware
+
+  # pass calls to the TestBench to the associated Middleware with PhpBuild and Host
+  # as prepended arguments.
+  #
+  # this ->    test_bench.call_script( )
+  # 
+  # becomes -> (test_bench.middleware).call_script( (test_bench.php), (test_bench.host), 'foo.php' )
+  #
+  [
+    :install!,
+    :uninstall!,
+    :apply_ini,
+    :call_script,
+  ].compact.each do |method_name|
+    define_method method_name do |*args,&block|
+      return middleware.method(method_name).call( php, host, *args, &block )
+    end
+  end
+
+
+  class << self
+    # TestBench.iterate( php_builds, hosts, middlewares, *args ){|*args| &block }
+    # 
+    # First three arguments must be TypedArray instances carrying collections of 
+    #  instances of PhpBuild, Host, and Middleware respectively. 
+    # 
+    # For each unique combination of PhpBuild, Host, and Middleware (all of which
+    # include TestBenchFactor), if the TestBenchFactors are compatible with each 
+    # other, the block is executed with the remaining arguments and its value is
+    # appended to the results array.
+    # 
+    # 
+    # 
+    def iterate( *args, &block )
+      php_builds, hosts, middlewares = args.shift(3)
+      results = []
+      php_builds.product hosts, middlewares do |php,host,middleware|
+        begin
+          test_bench = TestBench.new( php, host, middleware )
+          
+          #skip this test bench if its components are not compatible.
+          next unless test_bench.compatible?
+          
+          test_bench.install!
+
+          result = (yield *args)
+          results << result unless result.nil?
+        ensure
+          test_bench.uninstall!
+        end
+      end
+      results
+    end
+  end
+end
