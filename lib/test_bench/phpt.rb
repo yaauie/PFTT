611d3ccfc3f5ce6ca505843e246a735b811f9bb2
Ryan Biesemeyer <v-ryanbi@microsoft.com>
2011-09-06 11:18:00 -0700

    ensure cleanup happens

--- lib/test_bench/phpt.rb
+++ lib/test_bench/phpt.rb
@@ -26,82 +26,85 @@ module TestBench
 
     def run(test_case_sets)
       test_case_sets.map do |test_cases|
-        tmpdir = @host.mktmpdir(@middleware.docroot)
+        begin
+          tmpdir = @host.mktmpdir(@middleware.docroot)
 
-        # parse all the phpt's and upload the files at the same time.
-        threads = []
-        threads.push Thread.start{ puts 'uploading';@host.upload test_cases.path, tmpdir; puts 'uploaded.' }
-        threads.push Thread.start{ puts 'loading'; test_cases.load; puts 'loaded' }
-        threads.each {|thread| thread.join }
+          # parse all the phpt's and upload the files at the same time.
+          threads = []
+          threads.push Thread.start{ puts 'uploading';@host.upload test_cases.path, tmpdir; puts 'uploaded.' }
+          threads.push Thread.start{ puts 'loading'; test_cases.load; puts 'loaded' }
+          threads.each {|thread| thread.join }
 
-        results = self.class.results_array.new()
-        deployed = {}
-        test_cases.map do |test_case|
+          results = self.class.results_array.new()
+          deployed = {}
+          test_cases.map do |test_case|
 
-          @host.pushd File.join( 
-            tmpdir, 
-            File.basename( test_cases.path ), 
-            File.dirname( test_case.relative_path )
-          )
-          #puts 'cwd: '+@host.cwd
-          
-          test = test_case
-          puts "[    ] #{test.relative_path}"
-          # catch the result here
-          result_spec = catch(:result) do
-            # return early if this test case is not supported or is borked
-            case
-            when test.borked? then throw :result, [PhptTestResult::Bork]
-            when test.unsupported? then throw :result, [PhptTestResult::Unsupported]
-            end
-
-            begin
-
-              @middleware.apply_ini test.ini.to_a.map{|i|i.gsub('{PWD}',@host.cwd)} # ideal place for multithreading if @host.exec is threadsafe.
-              #puts '--INI--'
-              #puts test.ini.to_a.map{|i|i.gsub('{PWD}',tmpdir)}
-
-              # if a skipif section is present, see if we should skip this test
-              unless test[:skipif].nil?
-                deployed[:skipif] = deploy_phpt_section test, :skipif
-                skipif = @middleware.execute_php_script(deployed[:skipif])[1]
-                #puts '--SKIPIF--'
-                #puts test[:skipif]
-                if skipif.downcase.start_with? 'skip'
-                  throw :result, [PhptTestResult::Skip, skipif]
-                end
-                #puts '--SKIPIF-OUT--'
-                #puts skipif
+            @host.pushd File.join( 
+              tmpdir, 
+              File.basename( test_cases.path ), 
+              File.dirname( test_case.relative_path )
+            )
+            #puts 'cwd: '+@host.cwd
+            
+            test = test_case
+            puts "[    ] #{test.relative_path}"
+            # catch the result here
+            result_spec = catch(:result) do
+              # return early if this test case is not supported or is borked
+              case
+              when test.borked? then throw :result, [PhptTestResult::Bork]
+              when test.unsupported? then throw :result, [PhptTestResult::Unsupported]
               end
-              
+
               begin
-                # we did not skip the test, run it.
-                deployed[:file] = deploy_phpt_section test, :file
-                throw :result, [PhptTestResult::Meaningful, @middleware.execute_php_script( deployed[:file] )[1]]
+
+                @middleware.apply_ini test.ini.to_a.map{|i|i.gsub('{PWD}',@host.cwd)} # ideal place for multithreading if @host.exec is threadsafe.
+                #puts '--INI--'
+                #puts test.ini.to_a.map{|i|i.gsub('{PWD}',tmpdir)}
+
+                # if a skipif section is present, see if we should skip this test
+                unless test[:skipif].nil?
+                  deployed[:skipif] = deploy_phpt_section test, :skipif
+                  skipif = @middleware.execute_php_script(deployed[:skipif])[1]
+                  #puts '--SKIPIF--'
+                  #puts test[:skipif]
+                  if skipif.downcase.start_with? 'skip'
+                    throw :result, [PhptTestResult::Skip, skipif]
+                  end
+                  #puts '--SKIPIF-OUT--'
+                  #puts skipif
+                end
+                
+                begin
+                  # we did not skip the test, run it.
+                  deployed[:file] = deploy_phpt_section test, :file
+                  throw :result, [PhptTestResult::Meaningful, @middleware.execute_php_script( deployed[:file] )[1]]
+                ensure
+                  # and clean up if we are supposed to
+                  unless test_case[:clean].nil? or CONFIG[:skip_cleanup]
+                    deployed[:clean] = deploy_phpt_section test, :clean
+                    @middleware.execute_php_script deployed[:clean]
+                  end
+                end
+
               ensure
-                # and clean up if we are supposed to
-                unless test_case[:clean].nil? or CONFIG[:skip_cleanup]
-                  deployed[:clean] = deploy_phpt_section test, :clean
-                  @middleware.execute_php_script deployed[:clean]
+                deployed.values.flatten.each do |deployed_file|
+                  @host.delete deployed_file
                 end
               end
-
-            ensure
-              deployed.values.flatten.each do |deployed_file|
-                @host.delete deployed_file
-              end
             end
+
+            @host.popd
+
+            # take the caught result and build the proper object out of it
+            result = result_spec.shift.new( test, self, *result_spec )
+            puts result.to_s
+            result.save
+            results << result
           end
-
-          @host.popd
-
-          # take the caught result and build the proper object out of it
-          result = result_spec.shift.new( test, self, *result_spec )
-          puts result.to_s
-          result.save
-          results << result
+        rescue
+          @host.delete tmpdir
         end
-        @host.delete tmpdir
         results
       end
     end
